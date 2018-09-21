d3158264a87a9af3f81714405b1b157787ae9635
Damon J. Gray <djgray@damonjgray.org>
2018-09-21 05:50:57 -0700

    Added initial page layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # get 'pages/home'
+    root 'pages#home'
+    get 'about' => 'pages#about'
+    get 'contact' => 'pages#contact'
+    get 'blog' => 'pages#blog'
+    get 'extras' => 'pages#extras'
 end
